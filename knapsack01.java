80171d8516607fe2650473f4ffc8f58f0395cf71
nidhishagrawal <nidhish.agrawal@gmail.com>
2022-04-23 16:17:52 +0530

    Update knapsack01.java

--- knapsack01.java
+++ knapsack01.java
@@ -6,7 +6,7 @@ public int maxWeight (int [] wt, int [] val ,int W, int n) {
   if (W==0 || n==0) {
     return 0;
   }
-  if (wt[n-1]<W) {
+  if (wt[n-1]<=W) {
     return Math.max(val[n-1]+maxWeight(wt,val,W-wt[n-1],n-1)),maxWeight(wt, val, W, n-1);
   } else {
      return maxWeight(wt, val, W, n-1);
